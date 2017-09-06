f6b109d671963425afa431e7ce9b48ce3ecf1be3
Tim Campbell <timothylevicampbell@gmail.com>
2017-09-06 16:07:52 -0400

    Create basic Pokemon class

---
+++ src/components/pokemon.js
@@ -0,0 +1,7 @@
+class Pokemon {
+  constructor(name, frontSprite, backSprite) {
+    this.name = name
+    this.frontSprite = frontSprite
+    this.backSprite = backSprite
+  }
+}
--- src/components/pokemonList.js
+++ src/components/pokemonList.js
@@ -1,7 +1,9 @@
 class PokemonList {
   constructor(pokemons) {
     // Keep a reference of all the POKEMON
-    this.pokemons = pokemons
+    this.pokemons = pokemons.map(function(pokemon) {
+      return new Pokemon(pokemon.name, pokemon.sprites.front, pokemon.sprites.back)
+    })
     this.filteredPokemons = []
   }
 
@@ -17,15 +19,13 @@ class PokemonList {
 
   render() {
     const filteredPokemonTemplate = this.filteredPokemons.map(function renderPokemon(pokemon) {
-      let sprite = "front"
-
       return `
         <div class="pokemon-card" id="${pokemon.name}">
           <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
           <h1 class="center-text">${pokemon.name}</h1>
           <div style="width:239px;margin:auto">
             <div style="width:96px;margin:auto">
-              <img id="${pokemon.name}-img" src="${pokemon.sprites[sprite] }">
+              <img id="${pokemon.name}-img" src="${pokemon.frontSprite}">
             </div>
           </div>
           <p style="padding:10px;" class="center-text flip-image" data-pokename="${pokemon.name}" data-action="flip-image">flip card</p>
