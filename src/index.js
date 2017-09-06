9598991d458de780315bb52e2eb1e1f985394b11
Tim Campbell <timothylevicampbell@gmail.com>
2017-09-06 15:02:21 -0400

    Copy search event handler from procedural approach

--- src/index.js
+++ src/index.js
@@ -4,8 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
   const searchInput = document.getElementById('pokemon-search-input')
   const pokemonContainer = document.getElementById('pokemon-container')
 
-  searchInput.addEventListener('keyup', function() {
-    console.log('handle search on keyup')
+  searchInput.addEventListener('keyup', function(event) {
+    // 1. Get some input from the user
+    const searchTerm = event.target.value
+
+    // if (!searchTerm) {
+      // pokemonContainer.innerHTML = ''
+      // return
+    // }
+
+    // 2. Do some data manipulation / access
+    // const filteredPokemon = POKEMONS.filter(function(pokemon) {
+      // return pokemon.name.includes(searchTerm) // OR return pokemon.name.match(new RegExp(searchTerm))
+    // })
+
+    // 3. Render to the screen
+    // const filteredPokemonTemplate = filteredPokemon.map(renderPokemon).join('')
+    // pokemonContainer.innerHTML = filteredPokemonTemplate
   })
 
   pokemonContainer.addEventListener('click', function() {
