5ec2d816c9dc9ef56ebf0b1da860e9210abc9866
Tim Campbell <timothylevicampbell@gmail.com>
2017-09-06 16:33:50 -0400

    Refactor pokemon.js

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -1,17 +1,15 @@
 class Pokemon {
   constructor(name, frontSprite, backSprite) {
     this.name = name
-    this.currentImg = frontSprite
     this.frontSprite = frontSprite
     this.backSprite = backSprite
+    this.currentImg = frontSprite
   }
 
   flipImage() {
-    if (this.currentImg === this.frontSprite) {
-      this.currentImg = this.backSprite
-    } else {
-      this.currentImg = this.frontSprite
-    }
+    this.currentImg = this.currentImg === this.frontSprite
+      ? this.backSprite
+      : this.frontSprite
   }
 
   getElement() {
@@ -21,14 +19,15 @@ class Pokemon {
   render() {
     return `
       <div class="pokemon-card" id="${this.name}">
-        <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
-        <h1 class="center-text">${this.name}</h1>
-        <div style="width:239px;margin:auto">
-          <div style="width:96px;margin:auto">
-            <img id="${this.name}-img" src="${this.currentImg}">
+        <div style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc">
+          <h1 class="center-text">${this.name}</h1>
+          <div style="width:239px;margin:auto">
+            <div style="width:96px;margin:auto">
+              <img src="${this.currentImg}">
+            </div>
           </div>
-        </div>
-        <p style="padding:10px;" class="center-text flip-image" data-pokename="${this.name}" data-action="flip-image">flip card</p>
+
+          <p style="padding:10px;" class="center-text flip-image" data-pokename="${this.name}">flip card</p>
         </div>
       </div>`
   }
